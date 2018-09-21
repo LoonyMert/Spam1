ed30708f6162bd3fd57a9e83cd8f2e7ad23228ee
LoonyMert <43324618+LoonyMert@users.noreply.github.com>
2018-09-22 00:49:03 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', message => {
+if (message.content === "test") {
+  var ms = 120000;
+      var interval = setInterval (function () {
+        message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
+      }, ms); 
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
